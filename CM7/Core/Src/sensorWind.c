1efb8d97baefdead8dc946356aa4969e87280a28
ConnorStevens <conngstevens@gmail.com>
2026-04-08 18:13:42 -0600

    bunch of test prints added for UART testing

--- CM7/Core/Src/sensorWind.c
+++ CM7/Core/Src/sensorWind.c
@@ -54,7 +54,10 @@ void sensorWind_uart4Init(void) {
   UART4_Handler.Instance = UART4;
 
   HAL_UART_Init(&UART4_Handler);
-  printf("<----------------------------New Flash----UART INIT COMPLETE------------>");
+  printf("init done\r\n");
+  volatile uint32_t clk = HAL_RCC_GetPCLK1Freq();
+  printf("PCLK1: %u\r\n", (unsigned int)clk);
+  printf("<---New Flash--->\r\n");  printf("<----------------------------New Flash----UART INIT COMPLETE------------>");
 }
 
 /**
@@ -70,21 +73,30 @@ void sensorWind_rtosInit()
   */
 void sensorWind_handler(void *argument)
 {
-  uint8_t txBuffer[8] = {0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x39}; // protocol for asking windvane for wind direction in degrees
+  uint8_t txBuffer[8] = {0x02, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x39}; // protocol for asking windvane for wind direction in degrees
+  uint8_t txBuffer1[] = "Hello\r\n";
   uint8_t angleBuffer[7];
-  uint16_t rawAngle;
+  uint8_t num = 0;  
   HAL_StatusTypeDef error;
   for(;;)
   {
-    if (HAL_UART_Transmit(&UART4_Handler, txBuffer, 8, 1000) != HAL_OK) {
+    printf("<-------Entering For Loop--------->\r\n");
+    if (HAL_UART_Transmit(&UART4_Handler, txBuffer1, 8, 50) != HAL_OK) {
       printf("HAL transmit failed\r\n");
-    } 
-    if (error = HAL_UART_Receive(&UART4_Handler, angleBuffer, 7, 1000) != HAL_OK) {
-      printf("HAL receivefailed: %d\r\n", error);
     }
-    rawAngle = (uint16_t)(((angleBuffer[3] << 8) | angleBuffer[4]) / 10); 
-    servoSail_setAngle(rawAngle);
+    printf("<-------Entering receive Loop--------->\r\n");
+    // if (error = HAL_UART_Receive(&UART4_Handler, angleBuffer, 7, 1000) != HAL_OK) {
+    //   printf("HAL receivefailed: %d\r\n", error);
+    // }
+    // rawAngle = (uint16_t)(((angleBuffer[3] << 8) | angleBuffer[4]) / 10); 
+    // servoSail_setAngle(rawAngle);
+    if ((error = HAL_UART_Receive(&UART4_Handler, &num, 1, 10000)) != HAL_OK) {
+      printf("HAL receive failed: %d\r\n", error);
+    } else {
+      printf("Received: %c (dec=%d, hex=%x)\r\n", num, num, num);
+    }
 
+    printf("<--------- Exiting For Loop--------->\r\n");
     vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for demonstration purposes
   }
 }
