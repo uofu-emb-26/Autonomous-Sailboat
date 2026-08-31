f81dbdd980c2604edf26b5abebd362f71b24e3ac
Corbin Barney <corbbarney@gmail.com>
2026-08-31 16:10:29 -0600

    still missing a few files

--- CM7/Core/Src/battery.c
+++ CM7/Core/Src/battery.c
@@ -16,17 +16,20 @@ void battery_hardwareInit()
 
     // ADC_HandleTypeDef hadc1;
     // hadc1.Instance = ADC1;
-    // hadc1.Init.ClockPrescaler        = ADC_CLOCK_ASYNC_DIV2;
-    // hadc1.Init.Resolution            = ADC_RESOLUTION_16B;   // H7 supports up to 16-bit
-    // hadc1.Init.ScanConvMode          = DISABLE;
-    // hadc1.Init.ContinuousConvMode    = DISABLE;
-    // hadc1.Init.DiscontinuousConvMode = DISABLE;
-    // hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-    // hadc1.Init.NbrOfConversion       = 1;
-    // hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
-    // hadc1.Init.LowPowerAutoWait      = DISABLE;
-    // hadc1.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
-    // hadc1.Init.ConversionDataManagement = ADC_CONVERSIONDATA_DR;
+    // hadc1.Init.ClockPrescaler           = ADC_CLOCK_ASYNC_DIV4;   /* tune to your PER_CK */
+    // hadc1.Init.Resolution               = ADC_RESOLUTION_16B;
+    // hadc1.Init.ScanConvMode             = ADC_SCAN_ENABLE;
+    // hadc1.Init.EOCSelection             = ADC_EOC_SEQ_CONV;
+    // hadc1.Init.LowPowerAutoWait         = DISABLE;
+    // hadc1.Init.ContinuousConvMode       = ENABLE;
+    // hadc1.Init.NbrOfConversion          = NUM_CHANNELS;
+    // hadc1.Init.DiscontinuousConvMode    = DISABLE;
+    // hadc1.Init.ExternalTrigConv         = ADC_SOFTWARE_START;
+    // hadc1.Init.ExternalTrigConvEdge     = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    // hadc1.Init.ConversionDataManagement = ADC_CONVERSIONDATA_DMA_CIRCULAR;
+    // hadc1.Init.Overrun                  = ADC_OVR_DATA_OVERWRITTEN;
+    // hadc1.Init.LeftBitShift             = ADC_LEFTBITSHIFT_NONE;
+    // hadc1.Init.OversamplingMode         = DISABLE;
     // if (HAL_ADC_Init(&hadc1) != HAL_OK)
     // {
     //     Error_Handler();
